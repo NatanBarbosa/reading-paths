ecd4aee73029c84ce8333edecac4461d6c2a5654
Natan Barbosa <natanbarbosa525@gmail.com>
2022-06-29 22:36:03 -0300

    livro jogo quase terminado

--- scripts/livro.js
+++ scripts/livro.js
@@ -1,115 +1,289 @@
-const secao_leitura_livro = document.getElementsByClassName('secao-leitura-livro')[0];
-const btn_voltar = document.getElementById('btn-voltar');
-const btn_avancar = document.getElementById('btn-avancar');
-const livro = document.getElementById('livro');
+const historia = [
+    {
+        capitulo: 1,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>500.000 anos depois de Ódom, uma explosão cósmica que culminou no surgimento da galáxia, aconteceu um milagre. Por uma série de coincidências, surgiu-se um planeta muito rico em recursos minerais, chamado Vithetu, capaz de sustentar uma população de 1 trilhão de habitantes por centenas de trilhões de anos. </p>
+        <p>Em outra galáxia, havia outros dois planetas com suas respetivas populações. Eles estavam em paz, até o momento que descobriram que o planeta de ambos não poderia sustentar suas populações por mais de 5 anos.</p>
+        <p>Todavia, eles descobriram esse planeta milagroso que surgiu em outra galáxia, e assim começou uma rivalidade para ver quem iria dominá-lo primeiro e continuar com o reino vivo.</p>
+        `,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 2,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>Um desses reinos em decadência era o reino Lork, que estava voltado para no lado oeste. Nesse reino comandava Cratóris, conhecido como pai de todos. </p>
+        <p>Em Lork havia múltiplos tipos de habitantes, os ciborgues, humanos e outras espécies de seres vivos.</p>
+        <p>Cratóris Tinha uma filha que ele amava muito. Talia era uma ciborgue especial. Ela nasceu com o dom de combate e um espírito de guerreira, com um leve toque de teimosia. </p>
+        `,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 3,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>Outro desses reinos era o reino Waso. Esse reino havia apenas ciborgue e, os quais adoravam guerra, morte, destruição e conflitos. Todavia nem todos os habitantes desse reino concordavam com essa cultura, e por isso muitos se refugiavam no reino vizinho. Por causa disso, o Ditador desse reino ficava com raiva por seus habitantes ficarem fugindo de seus reinos. Por isso ele desenvolveu uma raiva de Lork e desejava sua decadência.</p>
+        <p>Conquistar o planeta Vithetu antes de Lork era a chance perfeita para acabar de vez com o reino Lork. </p>       
+        `,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 4,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>A fim de sair na frente nessa corrida espacial em direção à vithetu, o reino de Waso decidiu preparar um ataque surpresa ao reino de Lork, para destruir seus foguetes e incapacitá-los de saírem de seu planeta e conquistarem um novo. Eles estavam muito cientes de que perderiam muitos soldados e cidadãos nessa batalha, mas ainda estavam confiantes que com esse ataque surpresa, eles destruiriam qualquer chance de Lork conquistar Vithetu. O rei de Waso disse:</p>
+        <p>- vamos acabar de uma vez por todas com esses fracos de carne e osso. Somente a raça guerreira de Waso deve prevecer. VIVA A WASO!!!</p>
+        <p>-VIVAAA!!!</p>          
+        `,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 5,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>O que o reino de Waso não esperava um dos soldados para os quais havia sido anunciado o ataque surpresa deserdou de fininho e fugiu para o reino de Lork, porque dias depois descobriu que estava com a doença da ferrugem e perdeu completamente sua vontade de lutar. Apesar de ter sido caçado e levado vários tiros, ele conseguiu fugir para o outro reino. </p>       
+        `,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 6,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>Talia, filha de cratóris, tinha acabado de sair de uma briga contra imigrantes ilegais em uma cidade deserta, e nisso acabou se machucando e quebrando algumas peças. Por sorte ele conseguiu se consertar com o kit de emergência.</p>
+        <p>Voltando para sua cidade, ela ouviu um gatinho miando e tentou seguir o som. Ao encontrar o gato, se deparou com a cena de um veículo todo quebrado com um ciborgue machucado. Ela observou que esse ciborgue era do Reino inimigo, e já ficou alerta, mas logo viu que ele não tinha condições nem de andar. Ela tirou ela do carro e perguntou:</p>
+        <p>- Qual o seu nome? O que aconteceu com você?</p>
+        <p>- Meu nome não importa. Eu sou um ex-soldado de Waso, mas como você pode ver, não tenho muito tempo de vida. Além de estar com ferrugem, estou todo ferido.</p>
+        <p>- Vem cá, vou levar você para um hospital!</p>
+        <p>- Não precisa. Eu sou uma vergonha como desertor. Mas a verdade é que eu nunca gostei daquele Rei, então vou dar o troco a tudo o que ele me fez passar. Garota, ouça com atenção...</p>
+        <p>- Estou ouvindo</p>
+        <p>- Waso está planejando um ataque surpresa contra esse reino. Ele quer destruir todos os seus foguetes e impedirem vocês de ir para Vithetu. O ataque ocorrerá daqui 2 anos. A decisão é sua. Você pode acreditar em mim ou ignorar...</p>
+        <p>- Mas como assim? Por que Waso odeia tanto a gente????</p>
+        <p>- ...</p>
+        <p>O ciborgue já havia morrido.</p>`,
+        imagem: "capa-alita.jpg"
+    },
+    {
+        capitulo: 7,
+        nomeCap: "Nome do capítulo",
+        conteudo: `<p>Talia sai correndo para contar a informação para o pai.</p>
+        <p>- Pai, temos que nos preparar militarmente. Waso vai nos atacar de surpresa. Pare de investir nesses foguetes. Precisamos nos defender.</p>
+        <p>- FILHA!!! Nunca acredite nas palavras de um wasiano. Esse ciborgue estava mentindo para você tentar me manipular a atrasar a melhoria dos nossos foguetes e Waso sair na frente!</p>
+        <p>Talia acreditava fortemente nas palavras do ciborgue. Ele tinha um espírito de guerreiro, assim como Talia, e por isso ela entendia suas intenções e raiva contra aquele rei.</p>
+        <p>Por isso, Talia toma uma escolha</p>`,
+        imagem: "capa-alita.jpg",
+        escolhaParte: 1,
+        escolhas: 
+        [
+            {
+                texto: "desobedecer e atacar",
+                icone: `<i class="fa-solid fa-scalpel"></i>`
+            },
+            {
+                texto: "Obedecer e não atacar",
+                icone: `<i class="fa-solid fa-peace"></i>`
+            }
+        ]
+    },
+];
 
-const pag1 = document.getElementById('p1');
-const pag2 = document.getElementById('p2');
-const pag3 = document.getElementById('p3');
+const imgBox = document.querySelector("#img-box");
+const storyBox = document.querySelector("#story-box");
+const secaoMarcaPaginas = document.querySelector("#secao-marca-paginas");
+const btnOrChoose = document.querySelector("#btn-or-choose")
 
-const marca_pag_1 = document.getElementById('marca-pag-1');
-const marca_pag_2 = document.getElementById('marca-pag-2');
-const marca_pag_3 = document.getElementById('marca-pag-3');
-const marca_pag_4 = document.getElementById('marca-pag-4');
+let capAtual = 1;
+let maxCaps = historia.length;
 
-//eventListner dos botçoes
-btn_voltar.addEventListener('click', irPaginaAnterior);
-btn_avancar.addEventListener('click', irProximaPagina);
+function attMarcaPaginas(){
+    secaoMarcaPaginas.innerHTML = "";
 
-//configurações
-let pagAtual = 1;
-let numDePags = 3;
-let maxDePags = numDePags + 1;
-
-function abrirLivro(){
-    livro.style.transform = "translateX(50%)";
-    btn_voltar.style.transform = "translateX(-180px)";
-    btn_avancar.style.transform = "translateX(180px)";
-
-    secao_leitura_livro.style.alignSelf = 'flex-start';
-    secao_leitura_livro.style.width = '870px';
+    historia.forEach(cap => {
+        secaoMarcaPaginas.innerHTML += `<div class="numero-pagina ${cap.capitulo === capAtual ? 'atual' : ''}" id="marca-pag-${cap.capitulo}">
+        <span class="nome-capitulo">${cap.nomeCap}</span>
+        <strong>${cap.capitulo}</strong>
+    </div>`
+    })
 }
+attMarcaPaginas();
 
-function fecharLivro(estaNoComeco) {
-    if(estaNoComeco){
-        livro.style.transform = "translateX(0%)";
-    } else{
-        livro.style.transform = "translateX(100%)";
-    }
+//primeira página
+storyBox.innerHTML = `<h3>Capítulo ${historia[0].capitulo} - ${historia[0].nomeCap}</h3> ${historia[0].conteudo}`
+imgBox.innerHTML = `<img src="imagens/${historia[0].imagem}" class="img-fluid">`
 
-    btn_voltar.style.transform = "translateX(0px)";
-    btn_avancar.style.transform = "translateX(0px)";
+function nextCap(){
+    if(capAtual < maxCaps){
+        capAtual++
 
-    secao_leitura_livro.style.alignSelf = 'center';
-    secao_leitura_livro.style.width = 'auto';
-}
+        storyBox.innerHTML = `<h3>Capítulo ${historia[capAtual - 1].capitulo} - ${historia[capAtual - 1].nomeCap}</h3> ${historia[capAtual - 1].conteudo}`
+        imgBox.innerHTML = `<img src="imagens/${historia[capAtual - 1].imagem}" class="img-fluid">`
 
-function irProximaPagina(){
-    if (pagAtual < maxDePags){
-        switch (pagAtual){
-            case 1:
-                abrirLivro();
-                pag1.classList.add('virada');
-                pag1.style.zIndex = 1;
-
-                marca_pag_1.classList.remove('atual');
-                marca_pag_2.classList.add('atual');
-                break;
-            case 2:
-                pag2.classList.add('virada');
-                pag2.style.zIndex = 1;
-
-                marca_pag_2.classList.remove('atual');
-                marca_pag_3.classList.add('atual');
-                break;
-            case 3:
-                pag3.classList.add('virada');
-                pag3.style.zIndex = 1;
-                fecharLivro();
-
-                marca_pag_3.classList.remove('atual');
-                marca_pag_4.classList.add('atual');
-                break;
-            default:
-                throw new Error('Estado Desconhecido');
+        if(historia[capAtual - 1].escolhas){
+            changeBtnOrChoose("choose")
+        } else {
+            changeBtnOrChoose("btn")
         }
-        pagAtual++;
 
-        btn_voltar.scrollIntoView()
+        attMarcaPaginas();
     }
 }
 
-function irPaginaAnterior(){
-    if (pagAtual > 1){
-        switch (pagAtual){
-            case 2:
-                fecharLivro(true);
-                pag1.classList.remove('virada');
-                pag1.style.zIndex = 3;
+function previousCap() {
+    if(capAtual > 1){
+        capAtual--
 
-                marca_pag_2.classList.remove('atual');
-                marca_pag_1.classList.add('atual');
-                break;
-            case 3:
-                pag2.classList.remove('virada');
-                pag2.style.zIndex = 2;
+        storyBox.innerHTML = `<h3>Capítulo ${historia[capAtual - 1].capitulo} - ${historia[capAtual - 1].nomeCap}</h3> ${historia[capAtual - 1].conteudo}`
+        imgBox.innerHTML = `<img src="imagens/${historia[capAtual - 1].imagem}" class="img-fluid">`
 
-                marca_pag_3.classList.remove('atual');
-                marca_pag_2.classList.add('atual');
-                break;
-            case 4:
-                abrirLivro();
-                pag3.classList.remove('virada');
-                pag3.style.zIndex = 1;
+        attMarcaPaginas();
+    }
+}
 
-                marca_pag_4.classList.remove('atual');
-                marca_pag_3.classList.add('atual');
-                break;
-            default:
-                throw new Error('Estado Desconhecido');
+function changeBtnOrChoose(which){
+    if(which === "btn"){
+        btnOrChoose.innerHTML = `<button class="btn btn-success me-3" id="btn-previous"><i class="fa-solid fa-angle-left"></i></button>
+        <button class="btn btn-success" id="btn-next"><i class="fa-solid fa-angle-right"></i></button>`
+
+        document.querySelector("#btn-previous").addEventListener('click', previousCap);
+        document.querySelector("#btn-next").addEventListener('click', nextCap);
+    } else if(which === "choose"){
+        if(maxCaps <= historia[capAtual - 1].capitulo){
+            btnOrChoose.innerHTML = `<button class="btn btn-info me-3" onClick="makeChoose(${historia[capAtual - 1].escolhaParte}, '${historia[capAtual - 1].escolhas[0].texto}')">${historia[capAtual - 1].escolhas[0].texto} ${historia[capAtual - 1].escolhas[0].icone}</button>
+            <button class="btn btn-info" onClick="makeChoose(${historia[capAtual - 1].escolhaParte}, '${historia[capAtual - 1].escolhas[1].texto}')">${historia[capAtual - 1].escolhas[1].texto} ${historia[capAtual - 1].escolhas[1].icone}</button>`
         }
-        pagAtual--;
+    }
+}
+changeBtnOrChoose("btn");
 
-        btn_avancar.scrollIntoView()
+function makeChoose(parte, escolha){
+    if(parte === 1){
+        let continuacao = [];
+        if(escolha === "desobedecer e atacar"){
+            continuacao = [
+                {
+                    capitulo: 8,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Depois de discutir com o pai, Talia vai embora da sala e fica pensativa. Ela sabia que se não fizesse nada, seu reino não teria chances de sobreviver. Seu espirito de guerreira estava mais aceso do que nunca. Ela pensou:</p> <p>- O pessoal desse reino e o meu pai são muito pacifistas. Se eu não fizer algo, ninguém vi fazer e a gente não vai conseguir se defender do ataque surpresa. Por isso é nós que precisamos atacar primeiro, e imediatamente!</p> <p>- Será que eu posso contar com algumas pessoas para me ajudar? Ou é melhor eu fazer tudo sozinha?</p>`,
+                    imagem: "capa-alita.jpg",
+                    escolhaParte: 2,
+                    escolhas: [
+                        {
+                            texto: "Se preparar",
+                            icone: `<i class="fa-solid fa-people-group"></i>`
+                        },
+                        {
+                            texto: "Agir sozinha",
+                            icone: `<i class="fa-solid fa-user-secret"></i>`
+                        }
+                    ]
+                },
+            ]
+
+        } else {
+            continuacao = [
+                {
+                    capitulo: 8,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Tália escuta o seu pai, rei de Lork, mas ainda estava muito preocupada com a decisão de Cratóris, tal decisão deixou a guerreira bastante triste e então decidiu sair do reino, indo a um planeta afastando, onde, por conta da cultura acabou perdendo seu espírito de guerreira.</p> 
+                    <p>Ouve-se 2 anos de paz, e enquanto tudo estava na paz, o reino Lork não progrediu militarmente. Mas o reino Waso cada vez mais forte militarmente.</p>
+                    <p>Waso planejou um ataque perfeito, nesses 2 anos, para a queda de Lork. </p>
+                    <p>Com isso o reino Lork recebe a seguinte noticia de seus conselheiros, que Waso estava a poucas anos luz de seu reino e que estavam preparado para destruir todo o império.</p>
+                    <p>Cratóris ouvindo esse anúncio, fica bastante desesperado e ordena que todo reino, fique em prontidão, mas, que sabia que seu império não teria chances sobre Waso, já que não ouviu sua filha, Talia.</p>
+                    <p>Talia, ouvindo esse anuncio decidi voltar para Lork, para defender seus irmãos, mas não sabia  como, já que estava sem vontade e força de lutar.</p>
+                    <p>As 72 horas - luz o primeiro ataque começa ao império </p>
+                    <p>Os wasianos logo chegam à capital do reino. </p>
+                    <p>Talia, chegando já próxima ao reino, se depara com diversos soldados e cidadãos mortos e uma cena lê chama a atenção, um cidadão preste a ser morto por um wasiano.</p>
+                    <p>Então, passa na mente duas escolhas, ajudar o cidadão ou deixa-lo ser morto. </p>`,
+                    imagem: "capa-alita.jpg",
+                    escolhaParte: 3,
+                    escolhas: [
+                        {
+                            texto: "Salvar cidadão",
+                            icone: `<i class="fa-solid fa-shield"></i>`
+                        },
+                        {
+                            texto: "Deixa-lo",
+                            icone: `<i class="fa-solid fa-user-large-slash"></i>`
+                        }
+                    ]
+                },
+            ]
+        }
+
+        historia.push(continuacao[0]);
+        maxCaps = historia.length;
+        nextCap();
+    } else if(parte === 2){
+        let continuacao2 = [];
+        if(escolha === "Se preparar"){
+            continuacao2 = [
+                {
+                    capitulo: 9,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Me preparei</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+                {
+                    capitulo: 10,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Consegui capangas</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+            ]
+        } else {
+            continuacao2 = [
+                {
+                    capitulo: 9,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Não me preparei </p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+                {
+                    capitulo: 10,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Me lasquei</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+            ]
+        }
+
+        continuacao2.forEach(cap => {
+            historia.push((cap));
+        })
+        maxCaps = historia.length;
+        nextCap();
+    } else if(parte === 3){
+        let continuacao3 = [];
+        if(escolha === "Salvar cidadão"){
+            continuacao3 = [
+                {
+                    capitulo: 9,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Salvei cidadõ</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+                {
+                    capitulo: 10,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>ebaaaa</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+            ]
+        } else {
+            continuacao3 = [
+                {
+                    capitulo: 9,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Não salvei cidadão</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+                {
+                    capitulo: 10,
+                    nomeCap: "Nome do capítulo",
+                    conteudo: `<p>Se lascou kkkk</p>`,
+                    imagem: "capa-alita.jpg"
+                }, 
+            ]
+        }
+
+        continuacao3.forEach(cap => {
+            historia.push((cap));
+        })
+        maxCaps = historia.length;
+        nextCap();
     }
 }
